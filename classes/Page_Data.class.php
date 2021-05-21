c8e7ba9720ec1ed88daa861989556ae579e20b26
Sandip124 <chaudharypankaj.official@gmail.com>
2021-05-21 18:25:06 +0545

    basic layout for image upload completed

---
+++ classes/Page_Data.class.php
@@ -0,0 +1,12 @@
+<?php
+Class Page_Data{
+	public $title="";
+	public $content="";
+	public $css="";
+	public $embeddedStyle="";
+
+	public function addCSS($href){
+		$this->css .= "<link href='$href' rel='stylesheet' />";
+	}
+
+}
